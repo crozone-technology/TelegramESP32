fadc5311be3c02d0d03f4a03d172ab69b1e80860
YuTTYL <yuttapichai.leelai@gmail.com>
2025-01-30 22:07:04 +0700

    Update Basic.ino
    Remove unnecessary code

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -14,30 +14,27 @@
  3. Set WiFi credentials and bot token below
  4. Upload and monitor Serial
 
-    Created 26 January 2025
-    By YuTTYL
-    Modified 26 January 2025
-    By YuTTYL
-    
+ Created 26 January 2025
+ Report problems: yuttapichai.leelai@gmail.com
  Library: https://github.com/crozone-technology/TelegramESP32
 */
+
 #include <TelegramESP32.h>
-#include <WiFiClientSecure.h>
-#include <HTTPClient.h>
-#include "TelegramESP32.h"
 
 const char* deviceName = "WeatherStation#1";
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
 const char* BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11";
-const char* CHAT_ID = "-100xxxxxxxxxxxx";
+const char* CHAT_ID = "-100xxxxxxxxxxxx"; //Channel ID
+const char* PRIVATE_CHAT_ID = "xxxxxxxxxx"; //Private chat ID
+const char* GROUP_CHAT_ID = "-xxxxxxxxxx"; //Group ID
 
 TelegramESP32 telegram(BOT_TOKEN);
 
 void handleMessage(String& message) {
   if (message == "/temp") {
     float temp = 25.5;
-    telegram.sendMessage(String(deviceName) +"\nTemperature: " + String(temp) + "°C");
+    telegram.sendMessageToChat("Notification", String(deviceName) +"\nTemperature: " + String(temp) + "°C");
   }
 }
 
@@ -53,12 +50,13 @@ void setup() {
   telegram.begin();
   telegram.setMessageCallback(handleMessage);
   telegram.addChat(CHAT_ID, TelegramESP32::CHANNEL, "Notification"); 
-  // telegram.addChat("123456", TelegramESP32::PRIVATE, "Admin");
-  // telegram.addChat("-200xxx", TelegramESP32::GROUP, "Team");
+  // telegram.addChat(PRIVATE_CHAT_ID, TelegramESP32::PRIVATE, "Admin");
+  // telegram.addChat(GROUP_CHAT_ID, TelegramESP32::GROUP, "Team");
 
   char wifiIP[50];
   snprintf(wifiIP, sizeof(wifiIP), "Device Name: %s\nIP: %s", deviceName, WiFi.localIP().toString().c_str());
   telegram.sendMessageToChat("Notification",wifiIP);
+  // telegram.sendMessageToChat("Team",wifiIP);
 }
 
 void loop() {
